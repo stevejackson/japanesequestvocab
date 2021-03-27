d8592ffeec4a4fe01c8200dfa18c779d8def4398
Steve <steve@sjackson.net>
2021-03-27 03:05:25 -0400

    Create powerlevel CSV to MD script and import first files

---
+++ script/csv_to_md.rb
@@ -0,0 +1,32 @@
+require 'csv'
+
+# format:
+#
+# 強い,つよい,(adj-i) strong; powerful; mighty; potent; resistant; resilient; durable; (P),tsuyoi,,,,,,,
+#
+# 0: expression
+# 1: pronunciation
+# 2: definition
+CSV.foreach("./vocab_to_import.csv").with_index do |row, i|
+  power_level = i + 1
+  expression = row[0]
+  pronunciation = row[1]
+  definition = row[2]
+  # ---
+  # expression: 介抱
+  # definition: "(n,vs) nursing; looking after"
+  # notes: ''
+  # screenshot: ''
+  # pronunciation: かいほう
+  #
+  # ---
+
+  File.open("./testoutput/#{power_level.to_s.rjust(5, '0')}.md", "w") { |file|
+    file.puts "---"
+    file.puts "powerlevel: #{power_level}"
+    file.puts "expression: #{expression}"
+    file.puts "definition: #{definition}"
+    file.puts "pronunciation: #{pronunciation}"
+    file.puts "---"
+  }
+end
